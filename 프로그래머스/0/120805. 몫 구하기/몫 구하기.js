0ce936f6b694942725ce5df7d036ae8fbff560a6
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-11-19 15:42:03 +0900

    [level 0] Title: 몫 구하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.js
@@ -1,7 +1,4 @@
 function solution(num1, num2) {
-    var answer = 0;
-    if(0 < num1 <= 100 && 0 < num2 <= 100) {
-        answer = Math.floor(num1 / num2)
-    }
+    var answer = parseInt(num1 / num2);
     return answer;
 }
