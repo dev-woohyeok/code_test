a893fa4d7968c6373ee507d1f0ce188e4e0b7aff
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-11-20 14:13:39 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -5,13 +5,9 @@
 //    return Array(n).fill(1).map((num,i) => num * (n - i) );
 //}
 
-// 아... 자연수 n 임 쉣
 function solution(n) {
-    let answer = 0;
-    const digits = n.toString().split('');// 자리수 분리
-    // a-b 올림차순 b-a 내림차순
-    // a - b 가 음수면 a 가 양수면 b 가 옴 -> 이말을 해석하면 작은 값이 앞에 온다는거임 
-    // 1 ,3  1 - 3 은 음수 면 1이 앞에 3이 뒤에 그럼 작은 값이 앞에있음
-    answer = digits.reverse().map(Number);
+	let answer = 0;
+    const digits = n.toString().split(''); // 자리수 분리
+    answer = digits.reverse().map(Number); // 배열을 뒤집고, 스트링을 다시 정수로 변한
     return answer;
 }
