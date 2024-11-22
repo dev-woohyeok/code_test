dd66310aac46c948b20f1b15ed20a02b4bdf7d1e
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-11-22 17:25:27 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,13 +1,8 @@
-// 입력된 숫자가 짝수 n % 2 === 0 
-// 입력된 숫자가 홀수 n * 3 + 1
-// 1이 될떄까지 반복 while( n !== 1){}
-// 반복 횟수 반환이 필요함, steps 변수 추가
-// 500 횟수가 넘으면 -1 을 반환 
-
 function solution(num) {
+    if (num <= 0) return -1; // 0이나 음수는 처리 불가 (문제 조건에 따라 다를 수 있음)
+
     let steps = 0;
     while (num !== 1) {
-        if (steps > 500) return -1;
         num = num % 2 === 0 ? num / 2 : num * 3 + 1;
         steps++;
     }
