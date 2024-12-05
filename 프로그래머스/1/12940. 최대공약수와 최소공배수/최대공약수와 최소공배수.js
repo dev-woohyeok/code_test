c6c8b136aed977e5b2d0b9bb3c7da431f928efc9
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-12-05 16:52:34 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -9,17 +9,17 @@ function minDivisor(num1,num2){
     return result;
 }
 
-// 배수중에 가장 작은 숫자
-function maxMutiple(num1, num2, divisor){
-    // 최대 공약수가 1보다 크면 돌 중 큰 수
-    // 최대 공약수가 1보다 작으면 둘의 곱 -> 아님 뭘까? 
-    // 최소 공배수이면 둘다 배수인데, 2배 3배 4배 배수라는건 약수 아닌가? 
-    // 배수라는건 다른말고 그값이 약수라는거 최대 공약수로 두 값의 곱을 나누면 최대 공배수다
-    return divisor > 1 ? num1 * num2 / divisor : num1 * num2;
-}
+// // 배수중에 가장 작은 숫자
+// function maxMutiple(num1, num2, divisor){
+//     // 최대 공약수가 1보다 크면 돌 중 큰 수
+//     // 최대 공약수가 1보다 작으면 둘의 곱 -> 아님 뭘까? 
+//     // 최소 공배수이면 둘다 배수인데, 2배 3배 4배 배수라는건 약수 아닌가? 
+//     // 배수라는건 다른말고 그값이 약수라는거 최대 공약수로 두 값의 곱을 나누면 최대 공배수다
+//     return divisor > 1 ? num1 * num2 / divisor : num1 * num2;
+// }
 
 function solution(n, m) {
     const min = minDivisor(n,m);
-    const max = maxMutiple(n,m,min);
+    const max = n * m / min;
     return [min, max]
 }
