c806164226fcc1d593f0d13fcdf70a2ec4b613ee
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-12-08 17:59:18 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.30 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,9 +1,11 @@
 function solution(s) {
     return s.split(" ")
-            .map((str, idx) =>
-                 str.split("").map((str,idx) => idx % 2 === 0 
-                                        ? str.toUpperCase()
-                                        : str.toLowerCase()
-                                  ).join("")
-            ).join(" ");
+            .map((str, idx) => {
+                return str.split("")
+                          .map((str,idx) => {
+                                return idx % 2 === 0 
+                                            ? str.toUpperCase()
+                                            : str.toLowerCase()})
+                           .join("");
+            }).join(" ");
 }
