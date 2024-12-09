f72a78ccde3037410f75bbf71be60145ded0bf7e
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2024-12-09 16:21:30 +0900

    [level 1] Title: 최소직사각형, Time: 5.15 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,25 +1,14 @@
-// [[w,h],[w,h]]
-// max_w = 가로중 가장 긴거
-// max_idx = 가로중 가장 긴거의 Index
-// max_h = max_idx 세로 값 포함해서, 나머지 숫자중 가장 큰거
+// 둘중 큰것중에 가장 큰거 가로
+// 둘중 작은것중에 가장 큰거 세로 
 function solution(sizes) {
     let max_w = 0;
     let max_h = 0;
     
-    sizes.forEach((size) => {
-        const w = Math.max(size[0], size[1]);
-        if(w > max_w){
-            max_w = w; 
-            max_h = Math.min(size[0], size[1]);
-        }       
+    sizes.forEach(([w, h]) => {
+        const [width, height] = w > h ? [w, h] : [h, w];
+        max_w = Math.max(width, max_w);
+        max_h = Math.max(height, max_h);
     });
-    
-    sizes.forEach((size) => {
-        const h = Math.min(size[0], size[1]);
-        if(h > max_h){
-            max_h = h;
-        }
-    })
     
     return max_w * max_h;
 }
