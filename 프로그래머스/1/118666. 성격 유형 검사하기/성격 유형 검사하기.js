2364d48912ebae20b814f73da137919b3f138bb0
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2025-01-01 22:58:54 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 1.32 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,31 +1,20 @@
 function solution(survey, choices) {
-    const arr = [
-        "R","T",
-        "C","F",
-        "J","M",
-        "A","N",
-    ]; // 성격 지표
-    const scores = new Map(arr.map(key => [key, 0])); // 성격 지표 점수
-    const points = ["", 3, 2, 1, 0, 1, 2, 3]; // 점수 환산
+    const MBTIs = ["RT","CF","JM","AN"]; // MBTI 유형들
+    const scores = new Map(MBTIs.flatMap(([key1, key2]) => [[key1, 0], [key2, 0]])); // 점수 반영
     
-    for(const [i, pair] of survey.entries()){
-        const choice = choices[i]; // 선택지
-        const point = points[choice]; // 반영 점수
-        const [key1, key2] = [...pair]; // 매칭해야하는 점수
-        if(choice < 4){ // 점수 반영하기
-            scores.set(key1, scores.get(key1) + point);
-        }else if(choice > 4 ){
-            scores.set(key2, scores.get(key2) + point);
+    for(const [i, MBTI] of survey.entries()){ // 검사 반영
+        const choice = choices[i]; // 검사결과
+        const [key1, key2] = MBTI; 
+        if(choice < 4) {
+            scores.set(key1, scores.get(key1) + Math.abs(choice - 4));
+        }else if(choice > 4){
+            scores.set(key2, scores.get(key2) + Math.abs(choice - 4));
         }
     }
     
-    let result = "";
-    result += scores.get("R") >= scores.get("T") ? "R" : "T";
-    result += scores.get("C") >= scores.get("F") ? "C" : "F";
-    result += scores.get("J") >= scores.get("M") ? "J" : "M";
-    result += scores.get("A") >= scores.get("N") ? "A" : "N";
-    
-    return result;
+    return MBTIs
+    .map(([key1, key2]) => scores.get(key1) >= scores.get(key2) ? key1 : key2)
+    .join("");
 }
 // 풀이
 // 
