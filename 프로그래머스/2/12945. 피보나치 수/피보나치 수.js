fa84350e3084104a7eedb8696ee298e35463c50a
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2025-01-09 10:15:17 +0900

    [level 2] Title: 피보나치 수, Time: 1.65 ms, Memory: 36.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let prev = 0;
+    let curr = 1;
+    for(let i = 2; i <= n; i++){
+        const next = (prev + curr) % 1234567;
+        prev = curr;
+        curr = next;
+    }
+    return curr;
+}
