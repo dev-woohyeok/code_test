2c343601a4dd65dde8ea9a2fb9c15c172c5dfebb
dev_woo <74164437+dev-woohyeok@users.noreply.github.com>
2025-01-14 10:33:16 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+    // 최대공약수를 구하면서, 최소공배수를 배열로 순환하면서 구하면됨
+
+    const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+    
+    return arr.reduce((acc, cur) => lcm(acc, cur) ,1);
+}
